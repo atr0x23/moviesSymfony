8962537e7e26cd12be9661a08975596dd29f6f74
atr0x23 <atr0x23@gmail.com>
2022-12-01 10:33:53 +0200

    Save new objects to DB and showing them in the layout

---
+++ src/Controller/CreateMovieController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Movie;
+
+class CreateMovieController extends AbstractController
+{
+    #[Route('/create/movie', name: 'app_create_movie')]
+    public function index(): Response
+    {
+        return $this->render('create_movie/index.html.twig');
+    }
+
+//    #[Route('/movie', name: 'app_add_movie')]
+//    public function createMovie(ManagerRegistry $doctrine): Response {
+//
+//        $entityManager = $doctrine->getManager();
+//
+//        $movie = new Movie();
+//        $movie->setTitle('Harry Potter');
+//        $movie->setDescription('Of course it is happening inside your head, Harry, but why on earth should that mean that it is not real?');
+//        $movie->setUserId('1');
+//
+//        $entityManager->persist($movie);
+//        $entityManager->flush();
+//
+//        return new Response('New movie added with id' . $movie->getUserId());
+//      //  return $this->render('create_movie/index.html.twig', ['controller_name' => 'New movie add with id:', 'id' => $movie->getUserId()]);
+//    }
+
+}
--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use App\Repository\MovieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +12,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class MoviesController extends AbstractController
 {
     #[Route('/', name:'app_index')]
-    public function homepage():Response {
+    public function homepage(MovieRepository $movieRepository):Response {
 
+        $movies = $movieRepository->findAll();
 
-        return $this->render('list/movies.html.twig');
+        return $this->render('list/movies.html.twig', ['movies' => $movies]);
 
     }
+
 }
